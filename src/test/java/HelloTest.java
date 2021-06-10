a76cdb54c3f0522f3a250556c38a636e2364e6d6
ahmetcngr <ahmetcngr26@gmail.com>
2021-06-10 21:00:43 +0200

    added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
+
         System.out.println("Hello Test");
+        System.out.println("I am about to share this project on GitHub");
     }
 }
